a6a46efefcbbdc09af202b463ac77830c1d386f5
Andrew <andrewjhanson3@gmail.com>
2021-10-26 06:07:37 -0500

    setup background image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import {WeatherData} from './components/WeatherData'
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <WeatherData />
+    </div>
   );
 }
 
